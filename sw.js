375fd14c61ef93971b67eb7156b35edc0f067a0f
spenceradeyemi-collab <spenceradeyemi@gmail.com>
2026-02-08 05:57:02 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,55 +1,24 @@
-// Service Worker for YSI PWA
 
-const CACHE_NAME = 'ysi-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+const CACHE_NAME = 'ysi-cache-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'index.tsx',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Poppins:wght@300;400;600;700&display=swap'
 ];
 
-// Install event
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(error => {
-        console.log('Cache addAll error:', error);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// Fetch event
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-      .catch(error => {
-        console.log('Fetch error:', error);
-        return new Response('Offline - Resource not cached');
-      })
-  );
-});
-
-// Activate event
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
